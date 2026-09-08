0a7f4118ce6aef6450f5d2245bc18e6d376a673a
Lavita Pandey <lavita.pandeytechy@gmail.com>
2026-09-08 12:31:04 +0530

    Updated chatbot title

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-    apiBaseUrl: "/api/",
-  socketUrl: "/",
+  apiBaseUrl: "https://medgyn-dev-staging.azurewebsites.net/api/",
+  socketUrl: "https://medgyn-dev-staging.azurewebsites.net/",
   sockeytEndPointNotify: "chat/",
   widgetToken: "04fde0cc5c961225a990c748c24eab8171f8213c42e53832"
 };
